68e44e84fad46ee2e70cf164934a607752cd2232
benmeddah <handed@live.fr>
2020-12-02 21:28:13 +0000

    add tailwind with config

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div id="nav">
+  <div class="" id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
 </template>
-
+<script>
+import "tailwindcss/tailwind.css";
+export default {
+  name: "App"
+};
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  @apply w-full h-full bg-gray-400;
 }
 
 #nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  @apply bg-blue-800;
 }
 </style>
